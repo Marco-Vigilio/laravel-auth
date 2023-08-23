0a084eecbc6dd849d500e40ddbffacd8e3d790c8
Marco-Vigilio <tonyvigilio@gmail.com>
2023-08-23 15:52:00 +0200

    create migration & seeder Project

---
+++ database/seeders/ProjectSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class ProjectSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+        for ($i=0; $i < 100; $i++) { 
+            $newProject = new Progect();
+            $newProject->title = ucfirst($faker->unique()->words(4, true));
+            $newProject->content = $faker->paragraph(10, true);
+            $newProject->slug = $faker->slug();
+            $newProject->image = $faker->imageUrl(480, 360, "project", true, "projects", true, "png");
+            $newProject->save();
+        }
+    }
+}
