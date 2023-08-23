d689381fe44a478509800aacd55a29d5688bb75e
Marco-Vigilio <tonyvigilio@gmail.com>
2023-08-23 20:09:38 +0200

    create page create.blade

---
+++ resources/views/admins/project/create.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container" id="projects-conteiner">
+    <div class="row justify-content-center">
+        <div class="col-12">
+            <form action="" method="POST">
+            @csrf
+
+            <div class="mb-3">
+                <label for="exampleFormControlInput" class="form-label">
+                    Title
+                </label>
+                <input type="text" class="form-control" id="title" placeholder="Insert your project title" name="title">
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlInput" class="form-label">
+                    Image
+                </label>
+                <input type="text" class="form-control" id="image" placeholder="https://image.jpg" name="image">
+            </div>
+            <div class="mb-3">
+                <label for="content" class="form-label">
+                    Content
+                </label>
+                <textarea class="form-control" id="content" rows="7" name="content"></textarea>
+            </div>
+
+            <div class="mb-3">
+                <button type="submit">
+                    Create new post
+                </button>
+                <button type="reset">
+                    Reset
+                </button>
+            </div>
+            </form>
+        </div>
+    </div>
+</div>
+@endsection
